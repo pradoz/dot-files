cd54907229f03249089dc09b4143727881a19a9d
pradoz <45854435+pradoz@users.noreply.github.com>
2024-06-10 18:50:20 -0700

    Update fugitive.lua

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,12 +1,27 @@
+local cwd = vim.fn.getcwd()
+
+local branch
+if string.find(cwd, "mission_data_lake") then
+    branch = "main"
+elseif string.find(cwd, "goldfish%-hpc") then
+    branch = "main"
+elseif string.find(cwd, "velvet") then
+    branch = "trunk"
+end
+
 vim.keymap.set('n', '<leader>gg', vim.cmd.Git)
 vim.keymap.set('n', '<leader>gc', function() vim.cmd('Git commit') end)
 vim.keymap.set('n', '<leader>gp', function() vim.cmd('Git push') end)
-vim.keymap.set('n', '<leader>gr', function() vim.cmd('Git rebase -i dev') end)
-vim.keymap.set('n', '<leader>gw',
-function()
+vim.keymap.set('n', '<leader>gP', function() vim.cmd('Git push -fu') end)
+vim.keymap.set('n', '<leader>gr', function() vim.cmd('Git rebase -i ' .. branch) end)
+
+vim.keymap.set('n', '<leader>gw', function()
     vim.cmd('Git add -u')
     vim.cmd('Git commit -m "WIP"')
     vim.cmd('Git push')
 end)
 
-
+vim.keymap.set('n', '<leader>gW', function()
+    vim.cmd('Git add -u')
+    vim.cmd('Git commit -m "WIP"')
+end)
